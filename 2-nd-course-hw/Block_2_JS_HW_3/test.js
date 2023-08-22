a0a23f3bc37f4819887337679e77b35b9fd5709a
Your Name <you@example.com>
2023-08-22 18:58:43 +0300

    Сдаю 3 домашку с версткой и анимацией

--- 2-nd-course-hw/Block_2_JS_HW_3/test.js
+++ 2-nd-course-hw/Block_2_JS_HW_3/test.js
@@ -1,172 +1,76 @@
-// Block_HW_2-------------------------------------------------------------------------
-// let age = Number(prompt('Сколько вам лет?'));
-// let result = age + 17;
-// alert(`Через 17 лет вам будет ${result}`);
-
-// let salary = prompt('Введите Вашу ЗП');
-// let allMoney = salary / 100 * 87;
-// alert(typeof salary);
-// alert(typeof allMoney);
-// alert(allMoney);
-// console.log(`Деньги в твоем распоряжении ${allMoney}`);
-
-// let x=5;
-//  ++x;
-//  alert(x);
-
-// // Задание 1
-// let sky = 10;
-// alert(sky);
-// sky=20;
-// console.log(sky)
-
-
-// // Задание 2
-// let iphoneYear = 2007;
-// console.log(iphoneYear)
-
-// //Задание 3
-// let userNameJS = 'Brendanom Aikom';
-// console.log(userNameJS);
-
-// //Задание 4
-// let x = 10;
-// let y = 2;
-// alert(x + y);
-// alert(x - y);
-// alert(x * y);
-// alert(x / y);
-
-// //Задание 5
-// let result = 2**5;
-// alert(result);
-
-// //Задание 6
-// let a = 9;
-// let b = 2;
-// alert(a % b);
-
-// //Задание 7
-// let num = 1;
-// num +=5;
-// alert(num);
-
-// num -=3;
-// alert(num);
-
-// num *= 7;
-// alert(num);
-
-// num /= 3;
-// alert(num);
-
-// num +=1;
-// alert(num);
-
-// num -=1;
-// alert(num);
-
-// //Задание 8
-// let age = prompt("Сколько вам лет?");
-// alert(age);
-
-// // //Задание 9.0
-// let user = {
-// name: 'Tim',
-// age: 80,
-// isAdmin: true
-// }
-
-// //Задание 9.1
-// user["city of residence"]
-
-// //Задание 9.2
-// user.age = 100;
-
-// //Задание 9.3
-// delete user["city of residence"]
-
-// //Задание 9.4
-// let info = prompt("Какую информацию хотите узнать о пользователе?");
-// alert(user[info])
-
-// //Задание 10
-// let personName = prompt("Как Вас зовут?");
-// alert(`Привет, ${personName}!`)
-
 
 // Block_2_HW_3-------------------------------------------------------------------------
 
 // //Задание 1
 
-// let password = 'пароль';
-// let message = prompt('Введите пароль');
-// if (message === password) {
-// console.log('Пароль введен верно.');}
-// else {console.log('Пароль введен неправильно');} 
+let password = 'пароль';
+let message = prompt('Введите пароль');
+if (message === password) {
+console.log('Пароль введен верно.');}
+else {console.log('Пароль введен неправильно');} 
 
 //Задание 2
 
-// let c = Number(prompt('Введите число'));
-// if (c>0 && c<10) {console.log('Верно');}
-// else {console.log('Не верно');}
+let c = Number(prompt('Введите число'));
+if (c>0 && c<10) {console.log('Верно');}
+else {console.log('Не верно');}
 
 //Задание 3
 
-// let d = prompt('Введите первое число (e)');
-// let e = prompt('Введите первое число (d)');
+let d = prompt('Введите первое число (e)');
+let e = prompt('Введите первое число (d)');
 
-// if (d > 100 || e > 100) {console.log('Верно');}
-// else {console.log('Не верно');}
+if (d > 100 || e > 100) {console.log('Верно');}
+else {console.log('Не верно');}
 
 //Задание 4
 
-// let a = '2';
-// let b = '3';
-// // Код выше изменять менять нельзя, чтобы решить задачу исправьте код ниже: 
-// alert(Number(a) + Number(b));
+let a = '2';
+let b = '3';
+// Код выше изменять менять нельзя, чтобы решить задачу исправьте код ниже: 
+alert(Number(a) + Number(b));
 
 //Задание 5
-// let MonthNumber = Number(prompt('Введите номер месяца в году.'));
-// if (MonthNumber > 0 && MonthNumber < 13) {
-// switch (MonthNumber) 
-// {case 1 : console.log('Январь');
-// break;
+let MonthNumber = Number(prompt('Введите номер месяца в году.'));
+if (MonthNumber > 0 && MonthNumber < 13) {
+switch (MonthNumber) 
+{case 1 : console.log('Январь');
+break;
 
-// case 2 : console.log('Февраль');
-// break;
+case 2 : console.log('Февраль');
+break;
 
-// case 3 : console.log('Март');
-// break;
+case 3 : console.log('Март');
+break;
 
-// case 4 : console.log('Апрель');
-// break;
+case 4 : console.log('Апрель');
+break;
 
-// case 5 : console.log('Май');
-// break;
+case 5 : console.log('Май');
+break;
 
-// case 6 : console.log('Июнь');
-// break;
+case 6 : console.log('Июнь');
+break;
 
-// case 7 : console.log('Июль');
-// break;
+case 7 : console.log('Июль');
+break;
 
-// case 8 : console.log('Август');
-// break;
+case 8 : console.log('Август');
+break;
 
-// case 9 : console.log('Сентябрь');
-// break;
+case 9 : console.log('Сентябрь');
+break;
 
-// case 10 : console.log('Октябрь');
-// break;
+case 10 : console.log('Октябрь');
+break;
 
-// case 11 : console.log('Ноябрь');
-// break;
+case 11 : console.log('Ноябрь');
+break;
 
-// case 12 : console.log('Декабрь');
-// break;
-// }
-// }
-// else alert('Чепуха');
+case 12 : console.log('Декабрь');
+break;
+}
+}
+else alert('Чепуха');
 
 
