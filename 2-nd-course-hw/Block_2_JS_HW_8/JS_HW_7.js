af0b999403945999640442467055a9b45cd23c1f
Your Name <you@example.com>
2023-09-07 16:54:18 +0300

    Старт 8 домашки

---
+++ 2-nd-course-hw/Block_2_JS_HW_8/JS_HW_7.js
@@ -0,0 +1,224 @@
+
+///////////////////////////////////---------7 урок начало-----------///////////////////////////
+
+
+// function myFunction(v, w, x, y, z) {
+    
+// }
+// var args = [0, 1];
+// myFunction(-1, ...args, 2, ...[3]);
+// console.log(myFunction(args));
+
+// const ser1 = [1, 2, 3];
+// const ser2 = [9, 8, 7];
+// const allSer = [...ser1, 'i love JS',...ser2];
+// console.log(allSer);
+
+// let newMass = [].concat(allSer);
+// newMass.push('accelent JS');
+// newMass.unshift('start mass JS');
+// console.log(newMass);
+
+// function byPizza(price, ...rest) { // нахожу стоимость заказа пиццы посетителями
+//     return rest.map((quant) => {
+//         return price * quant;
+//     });
+// };
+
+// byPizza(500, 3, 4, 1, 2);
+// console.log(byPizza(100,1,2,3));
+
+
+        // // trening 1 .....................................toLowerCase()
+
+// const answerUser = prompt('Без рук, а рисует. Без зубов, а кусает.');
+// const answer = 'мороз';
+// if (answerUser.toLowerCase() === answer.toLowerCase()) {
+//     alert ('Это правильный ответ.')
+// } else { alert ('Не угадали.')}
+
+        // // trening 2 .....................................indexOf()
+
+// const str = 'Поезд едет далеко';
+// str.indexOf('едет'); //6 совпадение найдено с 6го индекса
+// str.indexOf('едит'); //вернет -1 т.к. совпадений не найдено
+
+
+        // // trening 2 .....................................includes(substr, pos)
+
+// const product = ['Хлеб Бородинский', 'Молоко Молочник', 'Коровка Молоко', 'Груша Конференция'];
+// const search = 'молоко';
+
+// product.forEach((product) => {
+//     if (product.toLowerCase().includes(search.toLowerCase())) {
+// console.log(product);
+//     }
+// }
+// );
+
+// // trening 3 .........................str.startsWith и str.endsWidth
+
+// const product = ['Хлеб Бородинский', 'Молоко Молочник', 'Коровка Молоко', 'Груша Конференция'];
+// const search = 'молоко';
+
+// product.forEach((product) => {
+//    if (product.toLowerCase().startsWith(search.toLowerCase())) {
+//     console.log(product);
+//    }
+// });
+
+// // trening 4 .........................split('').reverse().join('') !!!!!!!!!!!!!!!!!!!!
+
+// function palindrom(word) {
+//     return word.toUpperCase() === word.toUpperCase().split('').reverse().join('');
+// }
+// //palindrom('мим'); // true
+// palindrom('мак'); // false
+// // palindrom('заказ'); // true
+
+
+// //trening 5 
+// для начала создадим массив, в который занесем все дни недели на русском
+// помним, что отсчет в JS начинается с воскресенья - 0
+        //const days = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+// создадим массив, где будем хранить названия месяцев на русском
+        //const months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
+        //"Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+
+        // let myDate = new Date(); // здесь мы получаем текущую дату
+        // let fullDate = "Сегодня: " + myDate.getDate() + // getDate возвращает число
+        // " " + months[myDate.getMonth()] + 
+// getMonth возвращает номер месяца, который мы можем использовать в качестве индекса для массива months
+        //" " + myDate.getFullYear() + 
+// getFullYear возвращает год
+        //", " + days[myDate.getDay()]; 
+// getDay возвращает номер дня недели, который мы используем в качестве индекса для массива days
+
+        // console.log(fullDate); 
+// Сегодня: 1 Февраль 2023, Среда
+
+
+
+
+
+//         // //Zadacha 1 ---------------------------------------------------------
+//         // Преобразуйте строку js в верхний регистр JS.
+// let up = 'js';
+// up = up.toUpperCase();
+// console.log(up);
+
+
+//         // //Zadacha 2----------------------------------------------------------
+//         // Создайте функцию, которая в качестве параметров принимает массив строк и строку. Возвращать данная функция должна новый массив, содержащий только те элементы переданного массива, которые начинаются с переданной строки. Регистр символов не должен влиять.
+
+// const array = ['Кошка', 'Кит', 'Комар', 'Носорог']
+// const search = 'ко'
+
+// function searchStart (array, str){
+
+//     return array.filter(el => el.toLowerCase().startsWith(str.toLowerCase()));
+// }  
+// searchStart(['Кошка', 'Кит', 'Комар', 'Носорог'], 'ко');
+
+
+//         // //Zadacha 3
+//         // Округлите число 32.58884:
+//         // До меньшего целого
+//         // До большего целого
+//         // До ближайшего целого
+
+// let num = Number(32.58884);
+// console.log(Math.floor(num));
+// console.log(Math.ceil(num));
+// console.log(Math.round(num));
+
+//         // //Zadacha 4
+//         // Даны числа 52, 53, 49, 77, 21, 32. Необходимо найти среди этих чисел наименьшее и наибольшее числа и вывести их в консоль.
+
+//        // //1 способ........
+// let num = [52, 53, 49, 77, 21, 32];
+// console.log(Math.max.apply(null, num));
+// console.log(Math.min.apply(null, num));
+
+//         // //2 способ........
+// let num = [52, 53, 49, 77, 21, 32];
+// Math.max(...num);
+// Math.min(...num);
+
+
+//         // //Zadacha 5
+//         // Создайте функцию, которая будет выводить в консоль рандомное число от 1 до 10.
+// function num(numMin, numMax) {
+//         return Math.round(Math.random() * (numMax - numMin)) + numMin;
+//     }
+//     console.log(num(1, 10));
+
+//         // //Zadacha 6 --------------------------------------------
+//         // Напишите функцию, которая будет принимать на вход целое число, а возвращать массив случайных         целых чисел от 0 до переданного числа. Длина массива должна быть в 2 раза меньше переданного числа.
+
+// function startMass (array) {
+// let newMass = [];
+// for ( let i = 0; i < Math.floor(array/2); i++) {
+
+//     newMass.push(Math.floor(Math.random()*array));  
+// }
+// return newMass;
+// }
+
+// startMass(7); // [6, 2, 7]
+
+
+//         // //Zadacha 7
+//         // Напишите функцию, которая на вход принимает 2 целых числа, а в качестве результата возвращает случайное целое число в этом  диапазоне.
+// function num(numMin, numMax) {
+//         return Math.round(Math.random() * (numMax - numMin)) + numMin;
+//     }
+//     num(5, 15);
+
+//         // //Zadacha 8
+//         // Выведите в консоль текущую дату в стандартном режиме. Используйте один из трех рассмотренных в уроке способов.
+// let day = new Date();
+// console.log(day);
+
+//         // //Zadacha 9
+//         // // Создайте переменную currentDate и сохраните в нее текущую дату. Выведите дату, которая наступит через 73 дня после      текущей.
+
+// let currentDate = new Date();
+// let currentDateNew = (+currentDate);
+//  console.log(currentDate);
+// const days = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+// const months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
+// "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+// let days73 = 73 * 24 * 60 * 60 * 1000;
+// let serchDate = currentDateNew + days73;
+//         // console.log(currentDate);
+//         // console.log(days73);
+// let next73 = new Date(serchDate);
+// console.log(next73);
+
+                ////Вариант 2
+// let currentDate = new Date();
+// currentDate.setDate(currentDate.getDate()+73);
+// console.log(currentDate);
+
+
+//             // //Zadacha 10
+//             // Написать функцию, которая на вход принимает дату, а возвращает ее отображение в виде:
+//             // Дата: <число> <месяц на русском> <год> - это <день недели на русском>.
+//             /* Время: <часы>:<минуты>:<секунды> */
+//             /* Время, которое будет выведено, также хранится в объекте Date. */
+
+
+function convertformat(){
+const days = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+const months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+let curr_dt = new Date()
+let form_dt = "<" + curr_dt.getDate() +"> " + "<" + months[curr_dt.getMonth()] + "> " + "<" +curr_dt.getFullYear() +"> " + "- " + "это "+ "<" + days[curr_dt.getDay()] + ">";
+let form_tm = "<" + curr_dt.getHours() + ">:<" + curr_dt.getMinutes()+">:<" + curr_dt.getSeconds() +">";
+return [`${form_dt}, Время: ${form_tm}`];
+
+  }
+console.log(`Дата: ${convertformat()}`); 
+
+
+// ///////////////////////////////////---------7 урок КОНЕЦ-----------///////////////////////////
---
+++ 2-nd-course-hw/Block_2_JS_HW_8/test.js
@@ -0,0 +1,144 @@
+
+// Block_2_HW_5-------------------------------------------------------------------------
+
+// //Задание 1 
+// //Напишите функцию, которая возвращает меньшее из 2 чисел. Например, из двух чисел — 8 и 4 — функция должна вернуть 4. Если передадим 6 и 6, то функция должна вернуть 6.
+
+let num1 = prompt('Введите 1-e число.');
+let num2 = prompt('Введите 2-e число.');
+
+const littleNum = function () {
+    
+    return num1 < num2 ? alert(num1) : alert(num2);
+}
+
+littleNum();
+
+// //Задание 2
+// //Напишите функцию, которая принимает число, а возвращает строку 'Число четное', если число четное, 'Число нечетное' — если нечетное.
+let num = prompt('Введите 1-e число.');
+
+const strNum = function () {
+    
+    return num % 2==0 ? alert('Число четное') : alert('Число не четное') ;
+}
+
+strNum();
+
+//Задание 3
+// 3.1. Напишите функцию, которая принимает параметром число и выводит в консоль квадрат этого числа.
+
+//Способ 1
+const stepenNum = (num) => {
+    let result = num ** 2;
+    console.log(result);
+}
+stepenNum(3);
+
+//Способ 2
+const stepenNum = () => {
+    num = Number(prompt("Введите число."));
+    let result = num ** 2;
+    console.log(result);
+}
+stepenNum();
+
+// 3.2. Напишите функцию, которая принимает параметром число и возвращает квадрат этого числа.
+function stepenNum() {
+    let num = Number(prompt("Введите число."));
+    console.log(num ** 2);
+    return;
+}
+stepenNum();
+
+//Задание 4
+// Создайте функцию, которая:
+// Спрашивает у пользователя, сколько ему лет.
+// Если пользователь вводит отрицательное число, выводит на экран «Вы ввели неправильное значение».
+// Если пользователь введет число от 0 до 12, выводит на экран «Привет, друг!».
+// Если пользователь введет число больше или равно 13, выводит на экран «Добро пожаловать!».
+
+function question() {
+    let year = Number(prompt("Сколько Вам годков?"));
+    if (year > 0 && year <= 12) {alert('ПРивет друг.');}
+    else if (year >= 13) {alert('Добро пожаловать.');}
+    else {alert('Вы ввели неправильное значение.');}
+        return;
+    }
+    question();
+
+// //Задание 5
+//Напишите функцию, которая на вход принимает 2 числа:
+// Проверяет, являются ли переданные параметры корректными числами. (Преобразование типов и isNaN() помогут.)
+//     Если нет, то вернуть строку 'Одно или оба значения не являются числом'.
+//     Если оба параметра — числа, то вернуть произведение данных чисел.
+    function mult() {
+        num1 = prompt("Введите 1e число.");
+        num2 = prompt("Введите 2e число.");
+        
+        if (isNaN(num1) || isNaN(num2)) {
+         alert('Одно или оба значения не являются числом.');
+        }
+            else  {
+                return alert(num1 * num2);
+            }
+        }
+        mult();
+
+// // Задание 6
+
+
+const checkNumber = () => {
+    let number = Number(prompt("Введите число."));
+     if (isNaN(number)) {
+        alert('Переданный параметр не является числом.');
+     } else {
+        let mathOperation = number ** 3;
+        return console.log(`${number} в кубе равняется ${mathOperation}`);
+     }
+}
+
+checkNumber();
+
+// //Задание 7
+// // //Создайте объекты circle1 и circle2 со свойством 
+// radius
+// . У объектов должен быть методам 
+// getArea
+//  , которое возвращает площадь круга через радиус, а также 
+// getPerimeter
+//  , который возвращает периметр окружности.
+
+const cicle1 = {
+    radius: 4,
+    getArea() { 
+        let mathRadius = 3.1415926536 * cicle1.radius;
+         return mathRadius; 
+        },
+    getPerimeter() {
+        let mathPerimeter = 2 * cicle1.radius * 3.1415926536;
+         return mathPerimeter; 
+        }
+    }
+    
+    const cicle2 = {
+        radius: 8,
+        getArea() { 
+            let mathRadius = 3.1415926536 * cicle2.radius;
+             return mathRadius; 
+            },
+        getPerimeter() {
+            let mathPerimeter = 2 * cicle2.radius * 3.1415926536;
+             return mathPerimeter; 
+            }
+        }
+    
+        console.log(cicle1.getArea());
+        console.log(cicle1.getPerimeter());
+    
+        console.log(cicle2.getArea());
+        console.log(cicle2.getPerimeter());
+    
+    
+    
+    
